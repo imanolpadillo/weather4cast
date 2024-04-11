ca047111c8a209f9e6ecbeee7de852e546e9ada7
imanolpadillo <67315499+imanolpadillo@users.noreply.github.com>
2024-04-11 13:28:33 +0200

    Update weatherAPI2.py

--- weatherAPI2.py
+++ weatherAPI2.py
@@ -10,7 +10,7 @@ from enum import Enum
 # CONSTANTS AND GLOBAL VARIABLES
 # *************************************************************************************************** 
 
-DAYS = 5
+DAYS = 6
 
 class WeatherStatus(Enum):
     SUNNY = 1
@@ -34,9 +34,9 @@ dict_weather_status = [
 
 class DayWeather:
     def __init__(self, status=None, rain=None, temperature=None):
-        self.status = status if status is not None else ["-"]*24
-        self.rain = rain if rain is not None else ["0"]*24
-        self.temperature = temperature if temperature is not None else ["-"]*24
+        self.status = status if status is not None else [None]*24
+        self.rain = rain if rain is not None else ['0']*24
+        self.temperature = temperature if temperature is not None else [None]*24
 
 weekWeather = [DayWeather() for _ in range(DAYS+1)]  
 
@@ -72,102 +72,101 @@ def decode_json(data):
             first_temperature = item['main']['temp']
             first_status = item['weather'][0]['main']
         if item['dt_txt'][11:13] == "00":
-            weekWeather[day_index].temperature[0] = item['main']['temp']
-            weekWeather[day_index].temperature[1] = item['main']['temp']
-            weekWeather[day_index].temperature[2] = item['main']['temp']
+            weekWeather[day_index].temperature[0] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[1] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[2] = round(item['main']['temp'])
             weekWeather[day_index].status[0] = item['weather'][0]['main']
             weekWeather[day_index].status[1] = item['weather'][0]['main']
             weekWeather[day_index].status[2] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[0] = item['rain']['3h']
-                weekWeather[day_index].rain[1] = item['rain']['3h']
-                weekWeather[day_index].rain[2] = item['rain']['3h']
+                weekWeather[day_index].rain[0] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[1] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[2] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "03":
-            weekWeather[day_index].temperature[3] = item['main']['temp']
-            weekWeather[day_index].temperature[4] = item['main']['temp']
-            weekWeather[day_index].temperature[5] = item['main']['temp']
+            weekWeather[day_index].temperature[3] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[4] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[5] = round(item['main']['temp'])
             weekWeather[day_index].status[3] = item['weather'][0]['main']
             weekWeather[day_index].status[4] = item['weather'][0]['main']
             weekWeather[day_index].status[5] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[3] = item['rain']['3h']
-                weekWeather[day_index].rain[4] = item['rain']['3h']
-                weekWeather[day_index].rain[5] = item['rain']['3h']
+                weekWeather[day_index].rain[3] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[4] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[5] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "06":
-            weekWeather[day_index].temperature[6] = item['main']['temp']
-            weekWeather[day_index].temperature[7] = item['main']['temp']
-            weekWeather[day_index].temperature[8] = item['main']['temp']
+            weekWeather[day_index].temperature[6] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[7] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[8] = round(item['main']['temp'])
             weekWeather[day_index].status[6] = item['weather'][0]['main']
             weekWeather[day_index].status[7] = item['weather'][0]['main']
             weekWeather[day_index].status[8] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[6] = item['rain']['3h']
-                weekWeather[day_index].rain[7] = item['rain']['3h']
-                weekWeather[day_index].rain[8] = item['rain']['3h']
+                weekWeather[day_index].rain[6] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[7] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[8] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "09":
-            weekWeather[day_index].temperature[9] = item['main']['temp']
-            weekWeather[day_index].temperature[10] = item['main']['temp']
-            weekWeather[day_index].temperature[11] = item['main']['temp']
+            weekWeather[day_index].temperature[9] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[10] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[11] = round(item['main']['temp'])
             weekWeather[day_index].status[9] = item['weather'][0]['main']
             weekWeather[day_index].status[10] = item['weather'][0]['main']
             weekWeather[day_index].status[11] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[9] = item['rain']['3h']
-                weekWeather[day_index].rain[10] = item['rain']['3h']
-                weekWeather[day_index].rain[11] = item['rain']['3h']
+                weekWeather[day_index].rain[9] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[10] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[11] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "12":
-            weekWeather[day_index].temperature[12] = item['main']['temp']
-            weekWeather[day_index].temperature[13] = item['main']['temp']
-            weekWeather[day_index].temperature[14] = item['main']['temp']
+            weekWeather[day_index].temperature[12] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[13] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[14] = round(item['main']['temp'])
             weekWeather[day_index].status[12] = item['weather'][0]['main']
             weekWeather[day_index].status[13] = item['weather'][0]['main']
             weekWeather[day_index].status[14] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[12] = item['rain']['3h']
-                weekWeather[day_index].rain[13] = item['rain']['3h']
-                weekWeather[day_index].rain[14] = item['rain']['3h']
+                weekWeather[day_index].rain[12] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[13] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[14] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "15":
-            weekWeather[day_index].temperature[15] = item['main']['temp']
-            weekWeather[day_index].temperature[16] = item['main']['temp']
-            weekWeather[day_index].temperature[17] = item['main']['temp']
+            weekWeather[day_index].temperature[15] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[16] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[17] = round(item['main']['temp'])
             weekWeather[day_index].status[15] = item['weather'][0]['main']
             weekWeather[day_index].status[16] = item['weather'][0]['main']
             weekWeather[day_index].status[17] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[15] = item['rain']['3h']
-                weekWeather[day_index].rain[16] = item['rain']['3h']
-                weekWeather[day_index].rain[17] = item['rain']['3h']
+                weekWeather[day_index].rain[15] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[16] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[17] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "18":
-            weekWeather[day_index].temperature[18] = item['main']['temp']
-            weekWeather[day_index].temperature[19] = item['main']['temp']
-            weekWeather[day_index].temperature[20] = item['main']['temp']
+            weekWeather[day_index].temperature[18] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[19] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[20] = round(item['main']['temp'])
             weekWeather[day_index].status[18] = item['weather'][0]['main']
             weekWeather[day_index].status[19] = item['weather'][0]['main']
             weekWeather[day_index].status[20] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[18] = item['rain']['3h']
-                weekWeather[day_index].rain[19] = item['rain']['3h']
-                weekWeather[day_index].rain[20] = item['rain']['3h']
+                weekWeather[day_index].rain[18] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[19] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[20] = round(item['rain']['3h']*10)
         elif item['dt_txt'][11:13] == "21":
-            weekWeather[day_index].temperature[21] = item['main']['temp']
-            weekWeather[day_index].temperature[22] = item['main']['temp']
-            weekWeather[day_index].temperature[23] = item['main']['temp']
+            weekWeather[day_index].temperature[21] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[22] = round(item['main']['temp'])
+            weekWeather[day_index].temperature[23] = round(item['main']['temp'])
             weekWeather[day_index].status[21] = item['weather'][0]['main']
             weekWeather[day_index].status[22] = item['weather'][0]['main']
             weekWeather[day_index].status[23] = item['weather'][0]['main']
             if 'rain' in item:
-                weekWeather[day_index].rain[21] = item['rain']['3h']
-                weekWeather[day_index].rain[22] = item['rain']['3h']
-                weekWeather[day_index].rain[23] = item['rain']['3h']
+                weekWeather[day_index].rain[21] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[22] = round(item['rain']['3h']*10)
+                weekWeather[day_index].rain[23] = round(item['rain']['3h']*10)
             day_index+=1
         counter+=1
 
     # Replace empy temperature + status by first_perature and first_status
-    for x in range(len(weekWeather)):
-        for ycount, yvalue in enumerate(weekWeather[x].status):
-            if weekWeather[x].status[ycount] == '-':
-                weekWeather[x].status[ycount] = first_status
-                weekWeather[x].temperature[ycount] = first_temperature
+    for ycount, yvalue in enumerate(weekWeather[0].status):
+        if weekWeather[0].status[ycount] is None:
+            weekWeather[0].status[ycount] = first_status
+            weekWeather[0].temperature[ycount] = first_temperature
 
     # Decode weather status
     for x in range(len(weekWeather)):
@@ -182,10 +181,11 @@ def decode_weather_status(input_string, dict_list):
     :param dict_list: dictionary to compare input_string with.
     :return: key value from dict_list
     """
-    for dictionary in dict_list:
-        for key in dictionary:
-            if key.lower() in input_string.lower():
-                return dictionary[key]
+    if input_string is not None:
+        for dictionary in dict_list:
+            for key in dictionary:
+                if key.lower() in input_string.lower():
+                    return dictionary[key]
     return None
 
 
