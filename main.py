30298d9d5a0795c522d9240d14ccf3c810cf3509
imanolpadillo <imanolpadillo@gmail.com>
2024-04-13 11:10:24 +0200

    dummy test

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import weatherAPI
+from time import strftime
+import threading, time
 
-weatherAPI.refresh()
-print(weatherAPI.weekWeather[0].status)
+def thread_weatherAPI(f_stop):
+    print(strftime("%X"))
+    if not f_stop.is_set():
+        threading.Timer(10, thread_weatherAPI, [f_stop]).start()
+
+# start calling f now and every 60 sec thereafter
+f_stop = threading.Event()
+thread_weatherAPI(f_stop)
+time.sleep(60)
+f_stop.set()
+
+# weatherAPI.refresh()
+# print(weatherAPI.weekWeather[0].status)
+# print(strftime("%X"))
