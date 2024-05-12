7adee2dac944928cba6599b0b418eb488af0a641
imanolpadillo <imanolpadillo@gmail.com>
2024-05-12 17:41:56 +0200

    API1 uses apparent_temp

--- weatherAPI1.py
+++ weatherAPI1.py
@@ -12,7 +12,7 @@ from wlogging import LogType, LogMessage
 # CONSTANTS AND GLOBAL VARIABLES
 # *************************************************************************************************** 
  
-api_url = 'https://api.open-meteo.com/v1/forecast?latitude=42.85&longitude=-2.6727&hourly=temperature_2m,rain,weather_code,wind_speed_10m&wind_speed_unit=ms&timezone=auto'
+api_url = 'https://api.open-meteo.com/v1/forecast?latitude=42.85&longitude=-2.6727&hourly=apparent_temperature,rain,weather_code,wind_speed_10m&wind_speed_unit=ms&timezone=auto'
 api_name = 'openmet '
 
 dict_weather_status = [
@@ -89,7 +89,7 @@ def decode_json(data):
     count = 0
     for day in range(WeatherConfig.DAYS.value):
         for hour in range(24):
-            weekWeather[day].temperature[hour] = round(data['hourly']['temperature_2m'][count])
+            weekWeather[day].temperature[hour] = round(data['hourly']['apparent_temperature'][count])
             weekWeather[day].rain[hour] = ceil_half(data['hourly']['rain'][count])
             if (data['hourly']['wind_speed_10m'][count]>WeatherConfig.MAX_WIND_MS.value):
                 #wind status is not defined in 'weather code'
