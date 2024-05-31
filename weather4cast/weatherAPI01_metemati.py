f5f234d4b3c629d6045404004c58e4c92baf5a2a
imanolpadillo <imanolpadillo@gmail.com>
2024-05-31 16:32:38 +0200

    Update weatherAPI01_metemati.py

--- weather4cast/weatherAPI01_metemati.py
+++ weather4cast/weatherAPI01_metemati.py
@@ -154,7 +154,7 @@ def refresh():
         wlogging.log(LogType.ERROR.value, LogMessage.ERR_API_CONN.name, LogMessage.ERR_API_CONN.value + ': ' + str(e))
 
 
-# refresh() # get data first time
+refresh() # get data first time
 # print("METEMATI")
 # print(weekWeather[0].temperature)
 # print(weekWeather[0].status)
