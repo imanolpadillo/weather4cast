cab0cbd06007d7581c9e0eed3c04f72d56ddee4e
imanolpadillo <67315499+imanolpadillo@users.noreply.github.com>
2024-06-27 08:57:29 +0200

    add set_lifx_scene

--- weather4cast/lifx.py
+++ weather4cast/lifx.py
@@ -9,6 +9,7 @@ import configparser
 # CONSTANTS AND GLOBAL VARIABLES
 # *************************************************************************************************** 
 
+lifx_scenes = None   # List of saved scenes
 script_dir = os.path.dirname(os.path.abspath(__file__))
 secrets_file_path = os.path.join(script_dir, 'secrets.ini')
 config = configparser.ConfigParser()
@@ -17,24 +18,61 @@ config.read(secrets_file_path)
 # Your LIFX API token
 API_TOKEN = config['secrets']['LIFX_TOKEN']
 
-# The base URL for the LIFX API
-BASE_URL = "https://api.lifx.com/v1/lights/all/state"
-
 # The headers including the Authorization token
 headers = {
     "Authorization": f"Bearer {API_TOKEN}",
 }
 
+def get_lifx_scenes():
+    '''
+    get all lifx saved scenes
+    '''
+    global headers
+    # Endpoint to list all scenes
+    url = "https://api.lifx.com/v1/scenes"
+    
+    # Make a GET request to fetch scenes
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        scenes = response.json()
+        return scenes
+    else:
+        print(f"Failed to fetch scenes. Status code: {response.status_code}")
+        return None
+
+def set_lifx_scene(scene_name):
+    '''
+    set a lifx saved scene
+    input: scene_name (string)
+    '''
+    global lifx_scenes
+    global headers
+    scene = next((scene for scene in lifx_scenes if scene['name'] == scene_name), None)
+    if scene:
+        scene_uuid = scene['uuid']
+        url = f"https://api.lifx.com/v1/scenes/scene_id:{scene_uuid}/activate"
+
+        activate_response = requests.post(url, headers=headers)
+
+        if activate_response.status_code != 207:
+            print(f"Failed to activate scene '{scene_name}'. Status code: {activate_response.status_code}")
+    else:
+        print(f"Scene '{scene_name}' not found.")
+
 def rgb_to_hex(r, g, b):
     '''
-    Function to convert RGB to hexadecimal
+    Convert RGB to hexadecimal
     '''
     return f"#{r:02x}{g:02x}{b:02x}"
 
 def set_lifx_color(r, g, b, s):
     '''
-    Function to set LIFX color from RGB input
+    Set LIFX color from RGB input
     '''
+    global headers
+    # The base URL for the LIFX API
+    url = "https://api.lifx.com/v1/lights/all/state"
     # Get hex color
     hex_color = rgb_to_hex(r, g, b)
     # The payload to set the color to blue
@@ -46,12 +84,15 @@ def set_lifx_color(r, g, b, s):
     }
 
     # Make the request to the LIFX API to change the light color
-    response = requests.put(BASE_URL, headers=headers, json=payload)
+    response = requests.put(url, headers=headers, json=payload)
 
-    # # Check the response
-    # if response.status_code == 207:  # Multi-Status
-    #     print("The color has been set.")
-    # else:
-    #     print(f"Failed to set color: {response.status_code} - {response.text}")
+    # Check the response
+    if response.status_code != 207:  # Multi-Status
+        print(f"Failed to set color: {response.status_code} - {response.text}")
 
+
+lifx_scenes = get_lifx_scenes()
 # set_lifx_color(0, 0, 200, 0.8)
+# set_lifx_scene('stormy')
+
+
